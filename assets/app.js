47b5df7f3ab027ba5ff6face5c302bf7f1631743
wujiafeng0521 <wujiafeng0521@gmail.com>
2024-04-16 23:49:49 +0800

    seventeen commit

--- assets/app.js
+++ assets/app.js
@@ -14978,23 +14978,32 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var slick_carousel__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(slick_carousel__WEBPACK_IMPORTED_MODULE_0__);
 
 $('.product-slider').slick({
+  //产品滑块
+
   infinite: false,
-  responsive: [{
-    breakpoint: 1024,
-    settings: {
-      slidesToShow: 1,
-      infinite: true
-    }
-  }, {
-    breakpoint: 600,
-    settings: {
-      slidesToShow: 1,
-      infinite: true
-    }
-  }, {
-    breakpoint: 300,
-    settings: "unslick"
-  }]
+  //不是无限滚动
+  arrows: false,
+  //不显示循环
+  slidesToShow: 1,
+  //显示一张幻灯片
+  asNavFor: '.product-slider-nav' //指定了与当前轮播关联的导航轮播（.product-slider-nav）。意味着它们之间会同步滑动，当在主轮播中切换时，关联的导航轮播也会相应切换
+});
+$('.product-slider-nav').slick({
+  //产品导航滑块
+
+  infinite: true,
+  //无限滚动
+  arrows: false,
+  //不显示循环
+  slidesToShow: 3,
+  //显示3张幻灯片
+  asNavFor: '.product-slider',
+  //指定了与当前轮播关联的主轮播（.product-slider)
+  slidesToScroll: 1,
+  //一次滚动一张幻灯片
+  centerMode: true,
+  //启用中心模式
+  focusOnSelect: true //在导航元素上点击时，将焦点设置到相应的主轮播元素
 });
 
 /***/ }),
--- src/js/product.js
+++ src/js/product.js
@@ -1,27 +1,22 @@
 import "slick-carousel";
 
-$('.product-slider').slick({
+$('.product-slider').slick({//产品滑块
 
-    infinite: false,
+  infinite: false,//不是无限滚动
+  arrows: false,//不显示循环
+  slidesToShow:1,//显示一张幻灯片
+  asNavFor:'.product-slider-nav'//指定了与当前轮播关联的导航轮播（.product-slider-nav）。意味着它们之间会同步滑动，当在主轮播中切换时，关联的导航轮播也会相应切换
 
-    responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 1,
-            infinite: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            infinite: true
-          }
-        },
-        {
-          breakpoint: 300,
-          settings: "unslick"
-        }
-    ]
-  });
+});
+
+$('.product-slider-nav').slick({//产品导航滑块
+
+  infinite: true,//无限滚动
+  arrows: false,//不显示循环
+  slidesToShow:3,//显示3张幻灯片
+  asNavFor:'.product-slider',//指定了与当前轮播关联的主轮播（.product-slider)
+  slidesToScroll:1,//一次滚动一张幻灯片
+  centerMode: true,//启用中心模式
+  focusOnSelect: true//在导航元素上点击时，将焦点设置到相应的主轮播元素
+
+});
